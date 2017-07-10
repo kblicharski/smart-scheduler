735bf2b9ca418a7a7b80fb1d8366421d1143274c
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-09 20:43:12 -0500

    Adds my comments about the constraint solver

--- src/main.py
+++ src/main.py
@@ -31,9 +31,34 @@ problem = Problem()
 
 """
 Essentially, here is what we want to do:
-The user inputs a list of course numbers and their possible times.
-The solver then determines the solution space for all valid course combinations.
 
-E.g.
-1000: 
+The user inputs a list of course numbers they want to enroll in.
+We then fetch the list of Courses that are identified by these course numbers.
+Each course will then contain the times that it is running at.
+
+We will then add a variable for each Course in the course list, with its domain.
+The domain will be the list of possible time_block values the course is 
+offered at.
+
+e.g.
+user_input  [2230, 2210, 2630, 4172, 3700]
+(if a course with that course number does not exist, print an error)
+
+courses     [ ... ]
+
+We then define the Problem by adding each variable with its domain.
+The variables will be the Courses, and the domain will be the
+list of time_blocks associated with the Course, looking something
+like this:
+
+problem.addVariable(Course1: [12:30-13:30, 14:30-15:30, 12:00-13:00])
+...
+problem.addVariable(CourseN: [9:00-11:00, 12:00-14:00]
+
+After defining these variables and their domains, all we need to do is define a
+constraint for them -- this is the algorithm that the solver will use to build
+the solution space.
+
+This is the part I am really unsure of. After a cursory glance at the docs,
+I am not even sure if this is what the library was intended for.
 """
