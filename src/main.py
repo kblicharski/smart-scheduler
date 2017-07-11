01417b90d03afc80e9644180bf407954374455c2
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-11 00:14:06 -0500

    (WIP) Not sure why courses aren't being properly fetched

--- src/main.py
+++ src/main.py
@@ -25,12 +25,58 @@ def populate_courses():
 
     return courses
 
+
+def fetch_courses(course_input: list):
+    """
+    Fetches courses based on an input in the format `CS:2230`.
+    Raises an exception if a course was not found.
+
+    :param course_input: list of courses the user wants to enroll in
+    identified by their department and number
+    :return: list of identified courses, or an error if one was not found
+    """
+    fetched_courses = []
+
+    for course_id in course_input:
+        course_values = course_id.partition(':')
+        course_department = course_values[0]
+        course_number = course_values[2]
+
+        num_courses = len(fetched_courses)
+
+        for course in course_set:
+            print('{} == {}'.format(course.department, course_department))
+            print('{} == {}'.format(course.number, course_number))
+            if course.department == course_department and course.number == \
+                    course_number:
+                print('Found course' + course_id)
+                fetched_courses.append(course)
+
+        # We didn't actually find the course
+        if len(fetched_courses) == num_courses:
+            raise Exception('We could not find ' + course_id)
+
+    return fetched_courses
+
+
 problem = Problem()
 course_set = populate_courses()
-print("\n".join(str(x) for x in course_set))
 
+user_input = ['CS:2230', 'CS:2210', 'MATH:3700']
+invalid_user_input = ['CS:2230', 'ECE:2230']
+
+courses = fetch_courses(user_input)
+print(courses)
+# print("\n".join(str(x) for x in course_set))
+
+
+# This will work but then the library isn't useful anymore
 for course in course_set:
     problem.addVariable(course, course.time_blocks)
+
+# This won't work because lists aren't hashable
+# problem.addVariable(list(course_set), [course.get_time_block() for course in
+#                                  course_set])
 
 """
 Essentially, here is what we want to do:
