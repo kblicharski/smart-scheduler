76583c4d5337c17d778a6f534a1c64fc0a74f846
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-26 08:39:33 -0500

    Adds the ability to either manually input data fetching or do it from predefined inputs

--- src/main.py
+++ src/main.py
@@ -6,13 +6,49 @@ import time
 from api import get_course_sections
 from utils import log_course_sections, log_courses, create_course_groups
 
+
+def get_course_section_data(semester: int = None, departments: [str] = None) \
+        -> [dict]:
+    # TODO (docs)
+    """
+    :param semester:
+    :param departments:
+    :return:
+    """
+    # TODO: Add validation and error handling
+    # TODO: Add the capability to specify how many of each they want
+
+    if not semester:
+        print('What semester would you like to access course data from?')
+        print('69: Summer 2017')
+        print('68: Fall 2017')
+        print('70: Winter 2017')
+        print('71: Spring 2018')
+        semester = int(input())
+
+    if not departments:
+        print('What departments would you like to access? If multiple, add'
+              'each on a new line. Enter -1 to continue.')
+        departments = []
+        while True:
+            user_input = input()
+            if user_input == '-1':
+                break
+            departments.append(user_input)
+
+    course_section_data = []
+    for department in departments:
+        course_section_data += get_course_sections(semester, department)
+
+    return course_section_data
+
+
 start = time.time()
 
-course_section_data = get_course_sections(68, 'cs')
-course_section_data += get_course_sections(68, 'ece')
-course_section_data += get_course_sections(68, 'ie')
-course_section_data += get_course_sections(68, 'math')
+# semester = 68
+# departments = ['math', 'cs']
 
+course_section_data = get_course_section_data()
 log_course_sections(course_section_data)
 
 courses = create_course_groups(course_section_data)
@@ -21,37 +57,3 @@ log_courses(courses)
 end = time.time()
 
 print('Process took {} seconds'.format(end - start))
-
-'''
-def fetch_courses(courses: list):
-    """
-    Fetches courses based on an input in the format `CS:2230`.
-    Raises an exception if a course was not found.
-
-    :param courses: list of courses the user wants to enroll in
-    identified by their department and number
-    :return: list of identified courses, or an error if one was not found
-    """
-    fetched_courses = []
-
-    for course_id in courses:
-        course_values = course_id.partition(':')
-        course_department = course_values[0]
-        course_number = course_values[2]
-
-        num_courses = len(fetched_courses)
-
-        for course in course_set:
-            print('{} == {}'.format(course.department, course_department))
-            print('{} == {}'.format(course.number, course_number))
-            if course.department == course_department and course.number == \
-                    course_number:
-                print('Found course' + course_id)
-                fetched_courses.append(course)
-
-        # We didn't actually find the course
-        if len(fetched_courses) == num_courses:
-            raise Exception('We could not find ' + course_id)
-
-    return fetched_courses
-'''
